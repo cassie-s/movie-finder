a9e0a5aa8f5cd00c59e11b8850a86980c6f5dd1b
Collin-W <whalencollin@gmail.com>
2021-10-03 19:33:27 -0500

    movie data style, script touch up, code clean up

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -4,88 +4,79 @@ var getRandomGenre = function (genreId, genreText, genrePageId) {
     // $('#movie-results).text = "Showing movies in the " + genreText + " genre."    
 
     fetch('https://api.themoviedb.org/3/discover/movie?'
-    
-    + 'api_key=3e05de6918321bc70bb8260fdbd331f3'
 
-    + '&include_adult=false'
+            +
+            'api_key=3e05de6918321bc70bb8260fdbd331f3'
 
-    + '&with_genres='
+            +
+            '&include_adult=false'
 
-    + genreId
-    
-    + '&page='
-    
-    + genrePageId)
-                              
+            +
+            '&with_genres='
+
+            +
+            genreId
+
+            +
+            '&page='
+
+            +
+            genrePageId)
+
         .then(response => response.json())
         .then(response => {
             var genreList = [];
-            //note: i changed the curly braces for [] in genreList
-            for(var i = 0; i < 10; i++ ){
+            //note: Collin changed the curly braces to [] in genreList
+            for (var i = 0; i < 10; i++) {
                 var genreObject = {};
                 genreObject['tmdbId'] = response.results[i].id
                 genreObject['title'] = response.results[i].title;
                 genreObject['image'] = response.results[i].poster_path;
-                genreObject['rating'] = response.results[i].vote_average;//out of 10
+                genreObject['rating'] = response.results[i].vote_average; //out of 10
                 genreObject['description'] = response.results[i].overview;
                 genreList[i] = genreObject;
             }
             console.log(genreList);
             renderGenreMovies(genreList)
         });
-    
-    
+};
+
+function renderGenreMovies(genreList) {
+
+    for (var i = 0; i <= genreList.length; i++) {
+
+        
+        if (genreList[i].image === null) {
+            var imageUrl = "./assets/Images/null.jpg";
+        } else {
+            var imageUrl = "https://image.tmdb.org/t/p/w500" + genreList[i].image;
+        };
+
+        var cardImage = document.createElement('img');
+        cardImage.setAttribute("src", imageUrl);
+        cardImage.setAttribute("width", '50px');
+        cardImage.setAttribute("class", "card-image");
+        var setImgLocation = "main section div#" + i;
+        $(setImgLocation).html(cardImage);
+
+        var cardTitle = document.createElement('p');
+        cardTitle.textContent = genreList[i].title;
+        cardTitle.setAttribute("class", "card-title")
+        console.log(cardTitle);
+        $('#' + i).append(cardTitle);
+
+        var cardDescription = document.createElement('p');
+        cardDescription.textContent = genreList[i].description;
+        cardDescription.setAttribute("class", "card-description");
+        $('#' + i).append(cardDescription);
+
+        var cardRating = document.createElement('p');
+        cardRating.textContent = genreList[i].rating;
+        cardRating.setAttribute("class", "card-rating");
+        $('#' + i).append(cardRating);
     };
+};
 
-    function renderGenreMovies(genreList) {
-
-        for(var i = 0; i < genreList.length; i++) {
-
-            //var cardId = genreList[i].id
-            //console.log(cardId)
-
-            var cardTitle = document.createElement('h3')
-            cardTitle.textContent = genreList[i].title
-            console.log(cardTitle)
-            $('#' + i).append(cardTitle);
-
-
-            var cardImage = document.createElement('img');
-            var url = "https://image.tmdb.org/t/p/w500" + genreList[i].image;
-            console.log(url);
-            //cardImage.textContent = genreList[i].image
-            cardImage.setAttribute("src", url); //"https://image.tmdb.org/t/p/w500" + genreList[i].image
-            cardImage.setAttribute("width", '50px');
-            console.log(cardImage);
-            var nextObj = "main section div#" + i;
-            $(nextObj).html(cardImage);
-
-
-            // var html = $(cardImage).html()
-            // $('#' + i).append(html);
-
-
-            // if(genreList[i].image === null) {
-            //     //placeholder
-            // } else {}
-
-            var cardRating = document.createElement('p')
-            cardRating.textContent = genreList[i].rating
-            console.log(cardRating)
-            $('#' + i).append( cardRating )
-
-            var cardDescription = document.createElement('p')
-            cardDescription.textContent = genreList[i].description
-            console.log(cardDescription)
-            $('#' + i).append(cardDescription);
-
-
-            //var card = document.createElement('')
-            // card .textContent = genreList[i].
-
-        }
-    }
-    
 
 //start of function for getting combo-box value
 $('#genre-combo').change(function () {
@@ -97,11 +88,11 @@ $('#genre-combo').change(function () {
     var genreId = this.value;
 
     // set combobox text to value of selected option in combobox
-    var genreText = $( "#genre-combo option:selected" ).text();
+    var genreText = $("#genre-combo option:selected").text();
 
     // generate random integer between 1 and 500 for pageID, to mimic randomness
     var genrePageId = Math.floor(Math.random() * (500 - 2) + 1);
-    
+
     // send parameters to getRandomGenre function
     getRandomGenre(genreId, genreText, genrePageId);
 });
@@ -116,56 +107,60 @@ var getRandomMovies = function () {
     // $('#movie-results).text = "Showing movies in the " + genreText + " genre."    
 
     fetch('https://api.themoviedb.org/3/discover/movie?'
-    
-    + 'api_key=3e05de6918321bc70bb8260fdbd331f3'
 
-    + '&include_adult=false'
-   
-    + '&page='
-    
-    + moviePageId)
-                              
+            +
+            'api_key=3e05de6918321bc70bb8260fdbd331f3'
+
+            +
+            '&include_adult=false'
+
+            +
+            '&page='
+
+            +
+            moviePageId)
+
         .then(response => response.json())
         .then(response => {
             var movieList = {};
-            for(var i = 0; i < 10; i++ ){
+            for (var i = 0; i < 10; i++) {
                 var movieObject = {};
                 movieObject['tmdbId'] = response.results[i].id
                 movieObject['title'] = response.results[i].title;
                 movieObject['image'] = response.results[i].poster_path;
-                movieObject['rating'] = response.results[i].vote_average;//out of 10
+                movieObject['rating'] = response.results[i].vote_average; //out of 10
                 movieObject['description'] = response.results[i].overview;
                 movieList[i] = movieObject;
             }
             console.log(movieList);
         });
-    };
+};
 
-$('#refresh-btn').click(function(){
+$('#refresh-btn').click(function () {
     // set combobox ID to value of selected option in combobox
     var genreId = this.value;
-    
+
     // set combobox text to value of selected option in combobox
-    var genreText = $( "#genre-combo option:selected" ).text();
-        
+    var genreText = $("#genre-combo option:selected").text();
+
     // generate random integer between 1 and 500 for pageID, to mimic randomness
-     var genrePageId = Math.floor(Math.random() * (500 - 2) + 1);
-            
+    var genrePageId = Math.floor(Math.random() * (500 - 2) + 1);
+
     // send parameters to getRandomGenre function
     getRandomGenre(genreId, genreText, genrePageId);
 })
 
 // to-do: add function for "Get Random Movies" once ui elements in place
 
-$(document).ready(function(){
-        if ($('#genre-combo').value == null){
-           $('#refresh-btn').hide();
-           console.log("true" + "Value:" + $('#genre-combo').value) 
-        } else {
-            $('#refresh-btn').show(); 
-            console.log("false" + "Value:" + $('#genre-combo').value) 
-        };
-    });
+$(document).ready(function () {
+    if ($('#genre-combo').value == null) {
+        $('#refresh-btn').hide();
+        console.log("true" + "Value:" + $('#genre-combo').value)
+    } else {
+        $('#refresh-btn').show();
+        console.log("false" + "Value:" + $('#genre-combo').value)
+    };
+});
 
 //commented code abyss of cataclysm
 
@@ -181,4 +176,3 @@ $(document).ready(function(){
 //                       _//        {{
 //                    '='~'          \\_    
 //                                    ~~'
-
